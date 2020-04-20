c01a17a365f2e18009bad695ed8bb470fb298ca2
ManojOts <manoj.vg@ortusolos.com>
2020-04-20 16:46:03 +0530

    Subscription

--- ots-server/src/main/java/com/fuso/enterprise/ots/srv/server/dao/SubscriptionServiceDao.java
+++
@@ -1,8 +0,0 @@
-package com.fuso.enterprise.ots.srv.server.dao;
-
-import com.fuso.enterprise.ots.srv.api.service.request.AddSubscriptionBORequest;
-
-public interface SubscriptionServiceDao  {
-
-	 String addUserSubscription(AddSubscriptionBORequest addSubscriptionBORequest);
-}
